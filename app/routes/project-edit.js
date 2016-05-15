5c5968daa51faad21ce5d78183c0e75acd6debc5
Angelo Javonitalla <angelo.javonitalla@gmail.com>
2016-05-16 00:17:08 +0800

    defaulting to collapsed sidebar continued working on edit page adding hover effect on tables making wysig editor more dynamic

--- app/routes/project-edit.js
+++ app/routes/project-edit.js
@@ -1,4 +1,28 @@
 import Ember from 'ember';
 
+let project = {
+
+};
+
 export default Ember.Route.extend({
+  model(){
+    return {
+      header : {
+        title : "Stanford – Green Industry Courtyard",
+        navs : [
+          {
+            active : false,
+            title : "Project",
+            url : "project",
+            icon : "ion ion-easel"
+          },
+          {
+            active : true,
+            title: "Edit"
+          }
+        ]
+      },
+      project : project
+    };
+  }
 });
